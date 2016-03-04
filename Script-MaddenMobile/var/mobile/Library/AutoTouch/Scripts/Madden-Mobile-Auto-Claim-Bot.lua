ebbe0a7315f429a2df580901eae74070d84982af
egold555 <eric@golde.org>
2016-03-04 09:04:09 -0800

    Initial Release

---
+++ Script-MaddenMobile/var/mobile/Library/AutoTouch/Scripts/Madden-Mobile-Auto-Claim-Bot.lua
@@ -0,0 +1,18 @@
+CREATETIME="2015-10-18 21:01:33";
+SCREEN_RESOLUTION="640x1136";
+
+adaptOrientation(ORIENTATION_TYPE.LANDSCAPE_RIGHT);
+
+touchDown(4, 456, 957);
+usleep(99966.67);
+touchUp(4, 449, 976);
+usleep(650119.29);
+
+touchDown(2, 571, 886);
+usleep(116511.33);
+touchUp(2, 565, 894);
+usleep(650062.75);
+
+touchDown(7, 94, 37);
+usleep(149950.67);
+touchUp(7, 95, 26);
